38943c7339386c13a84be582534bb3607aa162e7
messi <418985958@qq.com>
2019-11-07 16:06:15 +0800

    denglu

--- src/main/java/messi/boot/study/bootstudy/controller/IndexController.java
+++ src/main/java/messi/boot/study/bootstudy/controller/IndexController.java
@@ -1,12 +1,35 @@
 package messi.boot.study.bootstudy.controller;
 
+import messi.boot.study.bootstudy.mapper.UserMapper;
+import messi.boot.study.bootstudy.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToekn(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
+
         return "index";
     }
 }
--- src/main/java/messi/boot/study/bootstudy/mapper/UserMapper.java
+++ src/main/java/messi/boot/study/bootstudy/mapper/UserMapper.java
@@ -3,10 +3,16 @@ package messi.boot.study.bootstudy.mapper;
 import messi.boot.study.bootstudy.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
-    void insertUser(User user);
+    void insertUser( User user);
+
+    @Select("select * from user where token = #{token}")
+     User findByToekn(@Param("token") String token);
 
 }
