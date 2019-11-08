f5f56934bd5191d740efd48e670b98c725f1085f
messi <418985958@qq.com>
2019-11-08 18:01:42 +0800

    增加文章

--- src/main/java/messi/boot/study/bootstudy/controller/AuthorizeController.java
+++ src/main/java/messi/boot/study/bootstudy/controller/AuthorizeController.java
@@ -7,7 +7,6 @@ import messi.boot.study.bootstudy.model.User;
 import messi.boot.study.bootstudy.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
